19e46ae2d4b83e5c8e4abc8c867bbf7150e61a20
NotAditya <90441096+NotAditya69@users.noreply.github.com>
2022-10-05 21:16:56 +0530

    Update Client.js

--- src/structures/Client.js
+++ src/structures/Client.js
@@ -3,6 +3,7 @@ const { connect } = require("mongoose");
 const { readdirSync } = require('node:fs');
 const Logger = require('@structures/Logger');
 const Cluster = require('discord-hybrid-sharding');
+const Manager = require('@structures/Manager');
 
 module.exports = class Moe extends Client {
     constructor() {
@@ -43,6 +44,7 @@ module.exports = class Moe extends Client {
             displayDate: true
         });
         this._connectMongodb();
+        this.manager = new Manager(this);
     }
     /**
      * 
